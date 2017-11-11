3ab27f32f01d54b5baf9f8f903e739b7b55efd37
ajstrider <abhinavhinger12@gmail.com>
2017-11-11 11:33:54 +0530

    Added Use profile with AVATAR bitch

--- portal/forms.py
+++ portal/forms.py
@@ -3,6 +3,9 @@ from django import forms
 # from .models import ForumPost, Comment, Messages, PostCategory
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.files.images import get_image_dimensions
+
+from .models import Profile
 
 
 # class PostForm(forms.ModelForm):
@@ -23,3 +26,47 @@ from django.contrib.auth.models import User
 # 	class Meta:
 # 		model = User
 # 		fields=('username','first_name','last_name','email','password1','password2',)
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('name', 'designation', 'office', 'residence' , 'education', 'workexp' ,'research')
+
+    def clean_avatar(self):
+        avatar = self.cleaned_data['avatar']
+
+        try:
+            w, h = get_image_dimensions(avatar)
+
+            #validate dimensions
+            max_width = max_height = 100
+            if w > max_width or h > max_height:
+                raise forms.ValidationError(
+                    u'Please use an image that is '
+                     '%s x %s pixels or smaller.' % (max_width, max_height))
+
+            #validate content type
+            main, sub = avatar.content_type.split('/')
+            if not (main == 'image' and sub in ['jpeg', 'pjpeg', 'gif', 'png']):
+                raise forms.ValidationError(u'Please use a JPEG, '
+                    'GIF or PNG image.')
+
+            #validate file size
+            if len(avatar) > (20 * 1024):
+                raise forms.ValidationError(
+                    u'Avatar file size may not exceed 20k.')
+
+        except AttributeError:
+            """
+            Handles case when we are updating the user profile
+            and do not supply a new avatar
+            """
+            pass
+
+        return avatar    
+
